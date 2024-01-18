dd54d92afb40dc6f636bbdca68df23607ad84aa9
CarlosEM3 <m.carlos.3e@gmail.com>
2024-01-18 12:03:58 -0800

    ceaser cipher

--- Python_project/caesar.py
+++ Python_project/caesar.py
@@ -1,9 +1,24 @@
+#The goal of ceasar cipher 
+# 1. Select some string value as your message. Label this string as “message”.
+# 2. Select some numeric key value to encode your message. Label this value as `k`.
+# 3. For each letter in “message”
+
+# a. Shift each letter in your message `k` letters forward.
+# b. If we go past the last letter in our alphabet (`z`), simply continue counting from
+# the first letter (`a`).
 import sys
 
-
+#encrypting a message 
 def encrypt(message, k):
-    return
+message = 
+encrypted_message = 
+for letters in message:
+    enletters + k
+    return encrypted_message
 
+character = 'a'
+numer = ord(character)
+print(numer)
 
 def decrypt(message, k):
     return
@@ -23,3 +38,7 @@ if __name__ == "__main__":
 
     print("Your encrypted word is", encrypted)
     print("Your decrypted word is", decrypted)
+
+character = 'a'
+numer = ord(character)
+print(numer)
